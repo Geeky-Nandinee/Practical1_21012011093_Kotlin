0cf6627a5dbf5ea35f210d526b5709f2c1d592c5
student <student@gnu.ac.in>
2023-07-24 08:51:18 +0530

    Practical 1

---
+++ Example1.kt
@@ -0,0 +1,8 @@
+class Student(val Enrollment_no:Int,var Name:String){
+
+}
+fun main(){
+    val a =Student (93,"Nandinee")
+    println(a.Name);
+    println(a.Enrollment_no)
+}
---
+++ Practical1.10.kt
@@ -0,0 +1,3 @@
+import java.security.acl.Owner
+
+class Car(var Type:String, var Model:String, val Price:Double, var owner: String)
